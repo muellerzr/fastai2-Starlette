edf69296d60703d7c833dbda81a3a253be36d1ae
Zachary Mueller <muellerzr@gmail.com>
2020-01-22 14:43:04 -0600

    Update server.py

--- app/server.py
+++ app/server.py
@@ -1,18 +1,17 @@
 import aiohttp
 import asyncio
 import uvicorn
-from fastai import *
-from fastai.vision import *
+from fastai2.vision.all import *
 from io import BytesIO
 from starlette.applications import Starlette
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = YOUR_GDRIVE_LINK_HERE
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = YOUR_CLASSES_HERE
 path = Path(__file__).parent
 
 app = Starlette()
@@ -32,7 +31,7 @@ async def download_file(url, dest):
 async def setup_learner():
     await download_file(export_file_url, path / export_file_name)
     try:
-        learn = load_learner(path, export_file_name)
+        learn = torch.load(path/export_file_name)
         return learn
     except RuntimeError as e:
         if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
@@ -57,11 +56,12 @@ async def homepage(request):
 
 @app.route('/analyze', methods=['POST'])
 async def analyze(request):
-    img_data = await request.form()
-    img_bytes = await (img_data['file'].read())
-    img = open_image(BytesIO(img_bytes))
-    prediction = learn.predict(img)[0]
-    return JSONResponse({'result': str(prediction)})
+  img_data = await request.form()
+  img_bytes = await (img_data['file'].read())
+  pred = learn.predict(BytesIO(img_bytes))[0]
+  return JSONResponse({
+      'results': str(pred)
+  })
 
 
 if __name__ == '__main__':
